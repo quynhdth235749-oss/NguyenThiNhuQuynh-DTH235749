6784429b6f1a22d2ac441d3851d970154a9e33a7
quynhdth235749-oss <quynh_dth235749@student.agu.edu.vn>
2025-09-07 20:50:12 +0700

    Add files via upload

---
+++ vidu_chuong1.py
@@ -0,0 +1,25 @@
+﻿from ctypes.util import test
+
+
+print('Hello, World')
+
+print('Albert Einstein đã từng nói: Một người không bao giờ mắc sai lầm không bao giờ thử bất kỳ điều gì mới.')
+
+if 5>2:
+    print('Five is greater than two')
+
+total=1+\
+    2+\
+    3
+print(total); print('Hello Python')
+
+if True:
+    print('True')
+else:
+    print('False')
+
+def add(a,b):
+    result=a+b
+    return result
+
+
